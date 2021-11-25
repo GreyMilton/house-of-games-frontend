873e6b3094b6b42d5a359d7dac0ae5890922529f
Grey Milton's MBP <grey.milton@gmail.com>
2021-11-25 15:12:03 +0000

    1. implements display of all parts of the currentReview in ReviewDisplay, 2. creates a pair of vote up/down buttons in ReviewDisplay which only console.log 'up' or 'down' to the console when clicked - more functionality to come when user is logged in (if I get there in the project)

--- src/components/ReviewDisplay.jsx
+++ src/components/ReviewDisplay.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getReviewById } from "../utils/api";
+import { capitaliseAndReplaceDashes } from "../utils/string-utils"
 
 function ReviewDisplay () {
-  const params = useParams();
-  const [currentReview, setCurrentReview] = useState(params.review_id);
+  const [currentReview, setCurrentReview] = useState();
   const [reviewDisplayIsLoading, setReviewDisplayIsLoading] = useState(true);
+  const params = useParams();
 
   useEffect(() => {
     console.log("reviewDisplayIsLoading:", reviewDisplayIsLoading);
@@ -17,7 +18,7 @@ function ReviewDisplay () {
 
   useEffect(() => {
     setReviewDisplayIsLoading(true);
-    getReviewById(currentReview).then((res) => {
+    getReviewById(params.review_id).then((res) => {
       setCurrentReview(res);
       setReviewDisplayIsLoading(false);
     }).catch((err) => {console.log(err)});
@@ -26,7 +27,19 @@ function ReviewDisplay () {
   return (
     <section className="review-display">
       {reviewDisplayIsLoading ? <p className="loading">loading...</p> : null}
-       <h2>{currentReview && currentReview.title }</h2> 
+      { currentReview ?
+      <>
+        <img className="review-image" src={currentReview.review_img_url} alt={currentReview.title} /><br/>
+        <h2>{currentReview.title }</h2>
+        <p>{currentReview.review_body}</p>
+        <p>Reviewer: {currentReview.owner}</p>
+        <p>At: {currentReview.created_at}</p>
+        <p>Game designer: {currentReview.designer}</p>
+        <p>Category: {capitaliseAndReplaceDashes(currentReview.category)}</p>
+        <p>Votes: {currentReview.votes} <button onClick={() =>{console.log("up")}}>+</button><button onClick={() => {console.log("down")}}>-</button></p>
+        <p>Comments: {currentReview.comment_count}</p>
+      </>
+      : null}
     </section>);
 }
 
