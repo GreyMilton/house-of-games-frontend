290a99c8673c21de6f27c8137feda089571e6308
Grey Milton's MBP <grey.milton@gmail.com>
2021-11-26 15:37:06 +0000

    creates all logic to successfully implement review votes button going up and down with optimistic rendering, WITHOUT yet being linked to the actual api

--- src/components/ReviewDisplay.jsx
+++ src/components/ReviewDisplay.jsx
@@ -1,8 +1,27 @@
 
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 import { capitaliseAndReplaceDashes } from "../utils/string-utils"
 
 function ReviewDisplay (props) {
-  
+  const [newVotesCount, setNewVotesCount] = useState(0);
+
+  useEffect(() => {
+    console.log("newVotesCount has been set:", newVotesCount)
+  }, [newVotesCount])
+
+  function addVotesToReview () {
+    setNewVotesCount((prevCount) => {
+      return prevCount + 1;
+    })
+  }
+
+  function subtractVotesFromReview () {
+    setNewVotesCount((prevCount) => {
+      return prevCount - 1;
+    })
+  }
+
   return (
     <section className="review-display">
       {props.reviewDisplayIsLoading ? <p className="loading">loading...</p> : null}
@@ -15,7 +34,7 @@ function ReviewDisplay (props) {
         <p>At: {props.currentReview.created_at}</p>
         <p>Game designer: {props.currentReview.designer}</p>
         <p>Category: {capitaliseAndReplaceDashes(props.currentReview.category)}</p>
-        <p>Votes: {props.currentReview.votes} <button onClick={() =>{console.log("up")}}>+</button><button onClick={() => {console.log("down")}}>-</button></p>
+        <p>Votes: {props.currentReview.votes + newVotesCount} <button onClick={addVotesToReview}>+</button><button onClick={subtractVotesFromReview}>-</button></p>
       </>
       : null}
     </section>);
