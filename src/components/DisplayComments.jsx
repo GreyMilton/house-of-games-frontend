e8212d079bd2084cc769b68e7439bd1b7a5fad68
Grey Milton's MBP <grey.milton@gmail.com>
2021-11-26 15:20:06 +0000

    successfuly gets delete comment to work with post and button linked up

--- src/components/DisplayComments.jsx
+++ src/components/DisplayComments.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { getCommentsByReviewId } from "../utils/api"
+import { deleteComment, getCommentsByReviewId } from "../utils/api"
 import { sortAndOrderArrayOfObjects } from "../utils/array-utils"
 import { UserContext } from "../contexts/user-context"
 
@@ -7,6 +7,7 @@ function DisplayComments (props) {
   const { currentUser, setCurrentUser } = useContext(UserContext)
   const [currentComments, setCurrentComments] = useState();
   const [commentsIsLoading, setCommentsIsLoading] = useState(true);
+  const [commentHasBeenDeleted, setCommentHasBeenDeleted] = useState(false);
 
   useEffect(() => {
     setCommentsIsLoading(true);
@@ -22,14 +23,22 @@ function DisplayComments (props) {
   },[props.params.review_id])
 
   useEffect(() => {
+    setCommentHasBeenDeleted(false);
     setCurrentComments((prevCurrentComments) => {
       const newCurrentComments = sortAndOrderArrayOfObjects(prevCurrentComments, props.currentCommentsSortBy, props.currentCommentsOrder);
       return newCurrentComments;
     })
-  }, [props.currentCommentsSortBy, props.currentCommentsOrder])
+  }, [commentHasBeenDeleted, props.currentCommentsSortBy, props.currentCommentsOrder])
 
-  function deleteComment(event) {
-    console.log("delete comment ", event.target.value);
+  function removeComment(event) {
+    console.log("deleting comment ", event.target.value);
+    deleteComment(event.target.value).then((res) => {
+      console.log(res);
+      setCommentHasBeenDeleted(true);
+    }).catch((err) => {
+      console.log(err);
+    })
+
   }
 
   return (
@@ -42,7 +51,7 @@ function DisplayComments (props) {
             <p>At: {comment.created_at}</p>
             <p>{comment.body}</p>
             <p>Votes:{comment.votes} <button onClick={() =>{console.log("up")}}>+</button><button onClick={() => {console.log("down")}}>-</button></p>
-            {( comment.author === currentUser) && <button value={comment.comment_id} onClick={deleteComment}>Delete comment</button>}
+            {( comment.author === currentUser) && <button value={comment.comment_id} onClick={removeComment}>Delete comment</button>}
             <p></p>
           </section>
         );
