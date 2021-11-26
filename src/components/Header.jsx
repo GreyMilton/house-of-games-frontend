73d89ba53230cd442525a4fe79d315f2642d98eb
Grey Milton's MBP <grey.milton@gmail.com>
2021-11-26 14:40:19 +0000

    creates button in Header that is a log-out button when user is logged in, but a log-in button when user is logged out. The button successfully logs the user out, and links to the log-in page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { useContext} from "react";
+import { UserContext } from "../contexts/user-context"
 
 function Header () {
+
+  const { currentUser, setCurrentUser } = useContext(UserContext)
+
+  function logOut () {
+    setCurrentUser(null);
+  }
+
   return (
     <header id="header">
       <Link to="/"><h1>Grey's House of Games</h1></Link>
-      <Link to="/login"><p>Login</p></Link>
+      {currentUser ? <p className="username-display">You are logged in as: {currentUser}</p> : null}
+      <Link to="/login"><button onClick={logOut}>{currentUser ? "Log out" : "Log in"}</button></Link>
     </header>);
 }
 
