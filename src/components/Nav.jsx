644bfa461e623782382a5be97563954506289b42
Grey Milton's MBP <grey.milton@gmail.com>
2021-11-30 19:57:03 +0000

    further develop nav with styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,18 +4,50 @@ import { useState } from "react";
 
 function Nav (props) {
 
-  const [showDropdown, setShowDropdown] = useState(true);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [navNumber, setNavNumber] = useState(7);
+  const [buttonState, setButtonState] = useState("not-clicked");
+  const [dropdownCategoryClicked, setDropdownCategoryIsClicked] = useState(false);
+
+  function clickOfButton () {
+    showDropdown ? setShowDropdown(false) : setShowDropdown(true);
+    setButtonState((prevState) => {
+      return (prevState === "not-clicked") ? "clicked" : "not-clicked";
+    })
+  }
+
+  function clickMainLink () {
+    setDropdownCategoryIsClicked(false);
+  }
+
+  function clickDropdownLink () {
+    setDropdownCategoryIsClicked(true);
+  }
 
   return (
-    <nav className="nav">
-      <button className="categories-button" onClick={() => {showDropdown ? setShowDropdown(false) : setShowDropdown(true)}} >Categories</button>      
-      {showDropdown && <NavLink to="/reviews/all">All categories</NavLink>}
-      {showDropdown && props.allCategories && props.allCategories.map((category,) => {
-        return (
-          <NavLink key={category.slug} to={`reviews/${category.slug}`}>{capitaliseAndReplaceDashes(category.slug)}</NavLink>
-        )
-      })}
-    </nav>);
+    <>
+      <nav className="nav-main">
+        <NavLink to="/reviews/all">All categories</NavLink>
+        {props.allCategories && props.allCategories.map((category, index) => {
+          if (index + 1 < navNumber) {
+            return (
+              <NavLink key={category.slug} to={`reviews/${category.slug}`} onClick={clickMainLink}>{capitaliseAndReplaceDashes(category.slug)}</NavLink>
+            )
+          }
+        })}
+       {navNumber < 8 && <button className={`categories-button ${buttonState} ${dropdownCategoryClicked ? "dropdown-clicked" : "main-clicked"}`} onClick={clickOfButton} >More categories...</button>}
+      </nav>
+      <nav className="nav-dropdown">    
+        {(showDropdown && navNumber < 1) && <NavLink to="/reviews/all">All categories</NavLink>}
+        {showDropdown && props.allCategories && props.allCategories.map((category, index) => {
+          if (index + 1 >= navNumber) {
+            return (
+              <NavLink key={category.slug + '-dropdown'} to={`reviews/${category.slug}`} onClick={clickDropdownLink}>{capitaliseAndReplaceDashes(category.slug)}</NavLink>
+            )
+          }
+        })}
+      </nav>
+    </>);
 }
 
 export default Nav;
