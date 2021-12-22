f41f3770b26ab5aa2f1e743e4adef3ea77367885
Grey Milton's MBP <grey.milton@gmail.com>
2021-12-22 17:29:40 +0000

    implemented basic log in page functionality

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,15 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/user-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function LoginPage () {
   const { currentUser, setCurrentUser, mostRecentUser, setMostRecentUser } = useContext(UserContext)
+  const navigate = useNavigate();
+  const [usernameDraft, setUsernameDraft] = useState();
+  const validUsernames = ['tickle122', 'grumpy19', 'happyamy2016', 'cooljmessy', 'weegembump', 'jessjelly'];
+  const [usernameIsValid, setUsernameIsValid] = useState(true);
+
+  const trackUsername = (event) => {
+    setUsernameDraft(event.target.value);
+  }
+
+  const submitUsernameForLogin = (event) => {
+    event.preventDefault();
+    if (validUsernames.includes(usernameDraft)) {
+      setUsernameIsValid(true);
+      setCurrentUser(usernameDraft);
+      setUsernameDraft();
+      event.target.reset();
+      navigate('/')
+    } else {
+      setUsernameIsValid(false);
+    }
+
+  }
+
 
   function handleLogInClick() {
     if (mostRecentUser) {
       setCurrentUser(mostRecentUser);
+      navigate('/')
     } else {
-      
+      setCurrentUser('hello!');
+      navigate('/lkjlkjlkjlkjlj')
     }
   }
 
@@ -34,16 +59,20 @@ function LoginPage () {
           { mostRecentUser ?
           <>
             <p>Welcome back {mostRecentUser}! Would you like to log in?</p>
-            <Link to="/" onClick={handleLogInClick}>Log In</Link>
+            <button to="/" onClick={handleLogInClick}>Log In</button>
             <br />
             <button onClick={handleNewUserClick}>New User</button>
           </>
           :
           <>
             <p>Please log in with a valid username</p>
-            <form>
-              <Link to="/" onClick={handleLogInClick}>Log In</Link>
+            <form onSubmit={submitUsernameForLogin}>
+              <label className="username-input-label">Username:
+                <input className="username-input" type="text" onChange={trackUsername}/>
+              </label>
+              <input className="username-submit" type="submit" value="Log In" />
             </form>
+            {!usernameIsValid && <p>Username in not valid!</p>}
           </>
           }
         </>
@@ -52,3 +81,10 @@ function LoginPage () {
 }
 
 export default LoginPage ;
+
+{/* <form onSubmit={submitNewComment}>
+<label className="new-comment-label">New comment:
+  <input className="new-comment-input" type="text" onChange={trackNewComment} />
+</label>
+<input className="new-comment-submit" type="submit" value="Submit" />
+</form> */}
