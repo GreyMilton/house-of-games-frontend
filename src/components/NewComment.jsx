6872cf136f28cd6230066a0da7b9842513b65473
Grey Milton's MBP <grey.milton@gmail.com>
2021-12-23 15:51:37 +0000

    prevent not logged in user from trying to post a comment whilst showing an error message

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -6,6 +6,7 @@ function NewComment (props) {
   const { currentUser } = useContext(UserContext)
   const [newCommentDraft, setNewCommentDraft] = useState("");
   const [newCommentIsBlank, setNewCommentIsBlank] = useState(false);
+  const [notLoggedInError, setNotLoggedInError] = useState(false);
 
   const trackNewComment = (event) => {
     setNewCommentDraft(event.target.value);
@@ -16,30 +17,36 @@ function NewComment (props) {
 
   const submitNewComment = (event) => {
     event.preventDefault();
-    if (newCommentDraft === undefined || newCommentDraft.length < 1) setNewCommentIsBlank(true);
-    postNewComment(props.params.review_id, currentUser, newCommentDraft).then((res) => {
-      props.setNewCommentCount(prevCommentCount => prevCommentCount + 1);
-      setNewCommentDraft();
-      event.target.reset();
-      props.setIsNetworkErrorReviewPage({});
-    })
-    .catch((err) => {
-      if (err.response && err.response.data.msg === "Incomplete request") {
-        setNewCommentIsBlank(true);
+    if (!currentUser) {
+      setNotLoggedInError(true);
+    } else if (newCommentDraft === undefined || newCommentDraft.length < 1) {
+      setNewCommentIsBlank(true);
+    } else {
+      postNewComment(props.params.review_id, currentUser, newCommentDraft).then((res) => {
+        props.setNewCommentCount(prevCommentCount => prevCommentCount + 1);
+        setNewCommentDraft();
+        event.target.reset();
         props.setIsNetworkErrorReviewPage({});
-      } else if (err.message === "Network Error") {
-        setNewCommentIsBlank(false);
-        props.setIsNetworkErrorReviewPage(prevState => {
-          const newObject = { ...prevState }
-          newObject.newComment = true;
-          return newObject;
-        });
-      }
-    });
+      })
+      .catch((err) => {
+        if (err.response && err.response.data.msg === "Incomplete request") {
+          setNewCommentIsBlank(true);
+          props.setIsNetworkErrorReviewPage({});
+        } else if (err.message === "Network Error") {
+          setNewCommentIsBlank(false);
+          props.setIsNetworkErrorReviewPage(prevState => {
+            const newObject = { ...prevState }
+            newObject.newComment = true;
+            return newObject;
+          });
+        }
+      });
+    }
   }
 
   return (
     <section className="new-comment-container">
+      { notLoggedInError && <p className="error-message">You must be logged in to add a new comment</p>}
       {props.isNetworkErrorReviewPage.newComment && <p className="error-message">Network Error</p>}
       {newCommentIsBlank && <p className="error-message">Your new comment is empty</p>}
       <section className="new-comment">
